baea2e11b572aedd521431cf1f74cfb68a5cb45d
REGIE <REGIE@LAPTOP-N9QJBTF3>
2024-03-15 14:07:21 +0800

    Engbino_Act3

---
+++ src/javaactivity3/JavaActivity3.java
@@ -0,0 +1,15 @@
+
+package javaactivity3;
+
+
+public class JavaActivity3 {
+
+    
+    public static void main(String[] args) {
+     double Celsius = 12;
+        double Fahrenheit = (Celsius * 9/5) + 32;
+        
+        System.out.println("Temperature in Fahrenheit is: " + Fahrenheit + "F");
+    }
+    
+}
